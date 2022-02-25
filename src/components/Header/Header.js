d9413ed6952fce399cbc944ad494cd7be1a3881c
Balint Erdi <balint@balinterdi.com>
2022-02-25 12:25:56 +0100

    Improvements for Step 2: Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
     <header>
       <SuperHeader />
       <MainHeader>
-        <Logo />
+        <Side>
+          <Logo />
+        </Side>
         <Nav>
           <NavLink href="/sale">Sale</NavLink>
           <NavLink href="/new">New&nbsp;Releases</NavLink>
@@ -21,22 +23,27 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
+        <Side />
       </MainHeader>
     </header>
   );
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 18px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
   display: flex;
-  height: 4em;
-  align-items: center;
+  height: 72px;
+  align-items: baseline;
+`;
+
+const Side = styled.div`
+  flex: 1;
 `;
 
 const Nav = styled.nav`
   display: flex;
-  margin: 0 auto;
+  margin: 0 48px;
   gap: 48px;
 `;
 
